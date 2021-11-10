3cccd9414005d252c9bccdd3f04991934cd84187
Venu Sai <venusaim23@gmail.com>
2021-11-10 21:34:13 +0530

    implemented View Binding

--- app/src/main/java/com/game/calculator/MainActivity.java
+++ app/src/main/java/com/game/calculator/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.game.calculator.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(binding.toolbarCalculator);
     }
 }
