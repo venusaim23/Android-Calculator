0b52cf4ff17d228f69a2864740cb4dee71fecb94
Venu Sai <venusaim23@gmail.com>
2021-11-11 16:23:48 +0530

    added functionality to buttons

--- app/src/main/java/com/game/calculator/MainActivity.java
+++ app/src/main/java/com/game/calculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.game.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.game.calculator.databinding.ActivityMainBinding;
 
@@ -10,12 +13,178 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private String displayTextString = "";
+    private double currentDisplayNumber = 0;
+    private boolean dot = false;
+    private double storedNumber = 0;
+
+    // 0 - None, 1 - percent, 2 - Divide, 3 - multiply, 4 - subtract, 5 - add
+    private int symbol = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbarCalculator);
+
+        currentDisplayNumber = Double.parseDouble(getResources().getString(R.string.default_string_display));
+
+        binding.button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(0);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(1);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(2);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(3);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(4);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(5);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(6);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(7);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(8);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (symbol == 0)
+                    appendString(9);
+                else {
+                    //store number and new line
+                }
+            }
+        });
+
+        binding.buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //insert dot
+            }
+        });
+
+        binding.buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (displayTextString.length() > 1) {
+                    displayTextString = displayTextString.substring(0, displayTextString.length() - 1);
+                    binding.displayTv.setText(displayTextString);
+                    currentDisplayNumber = Double.parseDouble(displayTextString);
+                } else if (displayTextString.length() == 1) {
+                    displayTextString = "";
+                    binding.displayTv.setText(getResources().getString(R.string.default_string_display));
+                    currentDisplayNumber = 0;
+                }
+            }
+        });
+
+        binding.buttonAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayTextString = "";
+                currentDisplayNumber = 0;
+                storedNumber = 0;
+                dot = false;
+                symbol = 0;
+                binding.displayTv.setText(getResources().getString(R.string.default_string_display));
+            }
+        });
+    }
+
+    private void appendString(int digit) {
+        if (digit == 0) {
+            if (currentDisplayNumber == 0)
+                return;
+        }
+        displayTextString = displayTextString.concat(Integer.toString(digit));
+        binding.displayTv.setText(displayTextString);
+        currentDisplayNumber = Double.parseDouble(displayTextString);
+        //
     }
 }
